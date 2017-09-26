d77bfd634e380d847c3681221b06ad758cbb9271
jhonathan98 <jhonathanandres98@gmail.com>
2017-09-26 17:48:00 -0500

    Update prueba.php
    1

--- prueba.php
+++ prueba.php
@@ -1,32 +1,38 @@
 <?php
+
 $url = 'https://api.sendgrid.com/';
 $user = 'jhonathan98';
 $pass = 'jhonathan123';
+
+$json_string = array(
+
+  'to' => array(
+    'jhonathan.cordoba4926@unaula.edu.co', 'alexiscalderon123@hotmail.com'
+  ),
+  'category' => 'test_category'
+);
 
 
 $params = array(
     'api_user'  => $user,
     'api_key'   => $pass,
-    'to'        => 'alexiscalderon123@hotmail.com',
-    'subject'   => 'Correo para ensayar Sendgrid',
-    'html'      => '<p> Correo de ensayo </p>',
-    'text'      => 'the plain text',
-    'from'      => 'example@pqrs.com',
+    'x-smtpapi' => json_encode($json_string),
+    'to'        => 'example3@sendgrid.com',
+    'subject'   => 'Testeo final',
+    'html'      => 'testing body',
+    'text'      => 'testing body',
+    'from'      => 'example@sendgrid.com',
   );
 
-print_r($params);
 
 $request =  $url.'api/mail.send.json';
 
 // Generate curl request
 $session = curl_init($request);
-
 // Tell curl to use HTTP POST
 curl_setopt ($session, CURLOPT_POST, true);
-
 // Tell curl that this is the body of the POST
 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-
 // Tell curl not to return headers, but do return the response
 curl_setopt($session, CURLOPT_HEADER, false);
 // Tell PHP not to use SSLv3 (instead opting for TLS)
