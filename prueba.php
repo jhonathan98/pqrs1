97d9feed619665238d54c6455935018b80093baf
jhonathan98 <jhonathanandres98@gmail.com>
2017-09-26 17:33:38 -0500

    Update prueba.php
    1

--- prueba.php
+++ prueba.php
@@ -1,42 +1,40 @@
 <?php
 
+Dotenv::load(__DIR__);
+$sendgrid_apikey = getenv('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');
+$sendgrid = new SendGrid($sendgrid_apikey);
 $url = 'https://api.sendgrid.com/';
-$user = 'jhonathan98';
-$pass = 'jhonathan123';
-
-$json_string = array(
-
-  'to' => array(
-    'alexiscalderon123@hotmail.com', 'jhonathan.cordoba4926@unaula.edu.co'
-  ),
-  'category' => 'test_category'
+$pass = $sendgrid_apikey;
+$template_id = '<your_template_id>';
+$js = array(
+  'sub' => array(':name' => array('Elmer')),
+  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
 );
 
-
 $params = array(
-    'api_user'  => $user,
-    'api_key'   => $pass,
-    'x-smtpapi' => json_encode($json_string),
-    'to'        => 'miguel-sg01@hotmail.com',
-    'subject'   => 'testing from curl',
-    'html'      => 'testing body',
-    'text'      => 'testing body',
-    'from'      => 'example@sendgrid.com',
+    'to'        => "miguel-sg01@hotmail.com",
+    'toname'    => "Example User",
+    'from'      => "miguel.sierragi@unaula.edu.co",
+    'fromname'  => "Your Name",
+    'subject'   => "PHP Test",
+    'text'      => "I'm text!",
+    'html'      => "<strong>I'm HTML!</strong>",
+    'x-smtpapi' => json_encode($js),
   );
-
 
 $request =  $url.'api/mail.send.json';
 
 // Generate curl request
 $session = curl_init($request);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
 // Tell curl to use HTTP POST
 curl_setopt ($session, CURLOPT_POST, true);
 // Tell curl that this is the body of the POST
 curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
 // Tell curl not to return headers, but do return the response
 curl_setopt($session, CURLOPT_HEADER, false);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
 // obtain response
